41507ee733997e46aa9fbf882a85aec8bf35bda5
uoy8 <mrtwaterr@gmail.com>
2026-03-22 21:16:00 +0200

    Enums in Domain/enums added

---
+++ apbd_eng_15_task3/Domain/Enums/EquipmentStatus.cs
@@ -0,0 +1,8 @@
+﻿namespace apbd_eng_15_task3.Domain.Enums;
+
+public enum EquipmentStatus
+{
+    Available,
+    Rented,
+    Unavailable
+}
---
+++ apbd_eng_15_task3/Domain/Enums/UserType.cs
@@ -0,0 +1,7 @@
+﻿namespace apbd_eng_15_task3.Domain.Enums;
+
+public enum UserType
+{
+    Student,
+    Employee
+}
