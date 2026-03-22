6abb0a26229d9cc613a40c0ad10dada8a00de0de
uoy8 <mrtwaterr@gmail.com>
2026-03-22 21:40:04 +0200

    Service/ equipment + user services were addedf

---
+++ apbd_eng_15_task3/Services/EquipmentService.cs
@@ -0,0 +1,43 @@
+﻿namespace apbd_eng_15_task3.Services;
+using apbd_eng_15_task3.Domain.Enums;
+using apbd_eng_15_task3.Domain.Equipment;
+
+public class EquipmentService
+{
+    private List<Equipment> _equipment = new List<Equipment>();
+
+    public void AddEquipment(Equipment equipment)
+    {
+        _equipment.Add(equipment);
+    }
+
+    public List<Equipment> GetAll()
+    {
+        return _equipment;
+    }
+
+    public List<Equipment> GetAvailable()
+    {
+        List<Equipment> result = new List<Equipment>();
+
+        foreach (Equipment equipment in _equipment)
+        {
+            if (equipment.IsAvailable())
+            {
+                result.Add(equipment);
+            }
+        }
+
+        return result;
+    }
+
+    public void MarkUnavailable(Equipment equipment)
+    {
+        if (equipment.Status == EquipmentStatus.Rented)
+        {
+            throw new InvalidOperationException(equipment.Name + " is currently rented and cannot be marked unavailable.");
+        }
+
+        equipment.Status = EquipmentStatus.Unavailable;
+    }
+}
---
+++ apbd_eng_15_task3/Services/UserService.cs
@@ -0,0 +1,17 @@
+﻿namespace apbd_eng_15_task3.Services;
+using apbd_eng_15_task3.Domain.Enums;
+using apbd_eng_15_task3.Domain.Equipment;
+public class UserService
+{
+    private List<User> _users = new List<User>();
+
+    public void AddUser(User user)
+    {
+        _users.Add(user);
+    }
+
+    public List<User> GetAll()
+    {
+        return _users;
+    }
+}
